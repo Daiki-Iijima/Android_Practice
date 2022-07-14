5322c1d9e4e7a1200551b0632169d4c4131c0233
Djima <djima.dev@gmail.com>
2022-07-15 00:14:54 +0900

    [add] スクロールビューと、ツールバーの使い方を簡単にテスト

---
+++ ToolbarSample/app/src/main/java/com/daiki/android/toolbarsample/MainActivity.java
@@ -0,0 +1,31 @@
+package com.daiki.android.toolbarsample;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.graphics.Color;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //  xmlで追加したToolbarの取得
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        //  ツールバーにアイテムを追加
+        //  ロゴ
+        toolbar.setLogo(R.mipmap.ic_launcher);
+        //  タイトル文字列と色を設定
+        toolbar.setTitle(R.string.toolbar_title);
+        toolbar.setTitleTextColor(Color.WHITE);
+        //  サブタイトルの文字列と色を設定
+        toolbar.setSubtitle(R.string.toolbar_subtitle);
+        toolbar.setSubtitleTextColor(Color.LTGRAY);
+
+        //  アクションバーにツールバーを設定
+        setSupportActionBar(toolbar);
+    }
+}
