0cf360183f897f69bcc2e483b374ee497e46e8e9
Djima <djima.dev@gmail.com>
2022-07-15 00:13:53 +0900

    [fix] カメラで撮影した画像を保存してから取得することで高画質で取得できるように

--- CameraIntentSample/app/src/main/java/com/daiki/android/cameraintentsample/MainActivity.java
+++ CameraIntentSample/app/src/main/java/com/daiki/android/cameraintentsample/MainActivity.java
@@ -4,14 +4,22 @@ import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentResolver;
+import android.content.ContentValues;
 import android.content.Intent;
-import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.ImageView;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class MainActivity extends AppCompatActivity {
+
+    //  ストレージに保存した画像のURI
+    private Uri mImageUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,17 +39,10 @@ public class MainActivity extends AppCompatActivity {
                     //  キャンセルされていたら処理を停止
                     if(result.getResultCode() == RESULT_CANCELED) {return;}
 
-                    //  データを取得してnullチェック
-                    Intent resultDataIntent = result.getData();
-                    if(resultDataIntent == null){ return; }
-
-                    //  撮影された画像のビットマップデータを取得
-                    Bitmap bitmap = resultDataIntent.getParcelableExtra("data");
-
                     //  取得した画像の設定
                     //  この方法だと、サムネイルサイズの低画質の画像しか取得できない
                     //  高画質画像を取得するには、ストレージを経由する必要がある
-                    ivCamera.setImageBitmap(bitmap);
+                    ivCamera.setImageURI(mImageUri);
                 }
         );
 
@@ -51,7 +52,7 @@ public class MainActivity extends AppCompatActivity {
 
     //  ImageViewがクリックされたとき
     //  CameraImageはxmlの方でカメラの画像をImageViewに設定しているので
-    private static class OnCameraImage implements View.OnClickListener{
+    private class OnCameraImage implements View.OnClickListener{
 
         ActivityResultLauncher<Intent> mLauncher;
 
@@ -61,7 +62,30 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public void onClick(View view) {
+            //  日時データ用のフォーマッタを作成
+            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMddHHmmss");
+            //  現在日時を秒の単位まで取得
+            Date now = new Date(System.currentTimeMillis());
+            //  取得したデータをフォーマッタを使用して整形
+            String nowStr = dateFormat.format(now);
+
+            String fileName = "CameraIntentSamplePhoto_" + nowStr + ".jpg";
+
+            ContentValues values = new ContentValues();
+            //  画像ファイル名を設定
+            values.put(MediaStore.Images.Media.TITLE,fileName);
+            //  画像ファイルの種類を設定
+            values.put(MediaStore.Images.Media.MIME_TYPE,"image/jpeg");
+
+            ContentResolver resolver = getContentResolver();
+
+            //  ContentResolverを使用してURIオブジェクトを生成
+            mImageUri = resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,values);
+
             Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+            //  Extra情報として生成したURI情報を付与
+            intent.putExtra(MediaStore.EXTRA_OUTPUT,mImageUri);
 
             //  アクティビティを起動
             mLauncher.launch(intent);
